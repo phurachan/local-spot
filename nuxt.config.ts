5a8ce209298eb1d4e11e2e21756ceee6353eb128
Phurachan <phurachan.p@gmail.com>
2025-09-26 23:10:45 +0700

    refactor deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,10 +29,21 @@ export default defineNuxtConfig({
     defaultLocale: 'th',
     strategy: 'prefix_except_default',
     langDir: 'locales/',
+    lazy: false,
     detectBrowserLanguage: {
       useCookie: true,
       cookieKey: 'i18n_redirected',
-      redirectOn: 'root'
+      redirectOn: 'root',
+      fallbackLocale: 'th'
+    },
+    experimental: {
+      jsTsFormatResource: true
+    }
+  },
+
+  nitro: {
+    experimental: {
+      wasm: true
     }
   },
   
